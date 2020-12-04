0718236f5d1e71eab43c61f4aaad351b46ae39ed
Ndn1618 <ndilbar1618@gmail.com>
2020-12-04 12:26:30 +0500

    Add lazy loading script

---
+++ js/lazy-loading.js
@@ -0,0 +1,13 @@
+(async () => {
+  if ('loading' in HTMLImageElement.prototype) {
+    const images = document.querySelectorAll("img.lazyload");
+    images.forEach(img => {
+      img.src = img.dataset.src;
+    });
+  } else {
+    // Dynamically import the LazySizes library
+    const lazySizesLib = await import('/lazysizes.min.js');
+    // Initiate LazySizes (reads data-src & class=lazyload)
+    lazySizes.init(); // lazySizes works off a global.
+  }
+})();
